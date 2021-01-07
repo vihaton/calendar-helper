48ec1d714c9f3559742db86fe8a5a2df1b642b2b
Vili Hätönen <vili@emblica.fi>
2021-01-08 00:14:39 +0200

    read and parse events, there is a bug with request timing

--- source/utils.py
+++ source/utils.py
@@ -10,6 +10,9 @@ from google.auth.transport.requests import Request
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
+SECRETS = '../secrets/'
+TOKEN = SECRETS + 'token.pickle'
+CREDS = SECRETS + 'credentials.json'
 
 def authenticate():
     '''
@@ -21,8 +24,8 @@ def authenticate():
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+    if os.path.exists(TOKEN):
+        with open(TOKEN, 'rb') as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
@@ -30,10 +33,10 @@ def authenticate():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                CREDS, SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
+        with open(TOKEN, 'wb') as token:
             pickle.dump(creds, token)
 
     return creds
